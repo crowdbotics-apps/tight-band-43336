181b16a139fff4ebce7f526f55a546c128b215c6
Crowdbotics <team@crowdbotics.com>
2023-09-11 18:47:41 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "tight-band-43336",
+  url: "https://tight-band-43336.botics.co",
+  api: "https://tight-band-43336.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
